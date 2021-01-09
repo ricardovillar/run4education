0fa466921028a54fb747c93c3e16fcc7d9549732
Ricardo Villar de Oliveira <ricardo.oliveira@induct.es>
2021-01-09 21:10:03 +0100

    update facebook share method

--- src/app/thank-you/thank-you.component.ts
+++ src/app/thank-you/thank-you.component.ts
@@ -23,9 +23,10 @@ export class ThankYouComponent implements OnDestroy {
   private _isTablet: boolean;
 
   contribution: JourneyContribution;
-  FB: any;
   id: any;
   whatsappSharing: string | SafeUrl;
+  contributionUrl: string;
+  facebookSharing: string | SafeUrl;
 
   constructor(
     route: ActivatedRoute,
@@ -33,7 +34,6 @@ export class ThankYouComponent implements OnDestroy {
     private store: Store<fromStore.State>,
     private sanitizer: DomSanitizer) {
     this.subscribeJourneyContributions();
-    this.initFacebook();
     route.queryParamMap.subscribe((params) => this.setContributionId(params.get('c')));
     this._isMobile = deviceService.isMobile();
     this._isTablet = deviceService.isTablet();
@@ -43,20 +43,39 @@ export class ThankYouComponent implements OnDestroy {
     this._subscriptions.forEach(s => s.unsubscribe());
   }
 
-  shareOnFacebook() {
-    const href = `${environment.MAP_URL}?c=${this.contribution._id}`;
-    const quote = this.getFacebookShareQuote();
-    this.FB.ui({
-      method: 'share',
-      quote: quote,
-      href: href
-    }, function (response) { });
+  private setContributionId(id: any) {
+    this.id = id;
+    this.setContribution();
+  }
+
+  private subscribeJourneyContributions() {
+    let sub = this.store.select(fromRoot.getJourneyContributions)
+      .subscribe((contributions: JourneyContribution[]) => {
+        this._journeyContributions = contributions;
+        this.setContribution();
+      });
+    this._subscriptions.push(sub);
+  }
+
+  private setContribution() {
+    if (!this.id || !this._journeyContributions) {
+      return;
+    }
+    this.contribution = this._journeyContributions.find(c => c._id == this.id);
+    if (!this.contribution) {
+      return;
+    }
+    this.contributionUrl = `${environment.MAP_URL}?c=${this.contribution._id}`;
+    this.setupFacebookSharing();
+    this.setupWhatsappSharing();
+  }
+
+  private setupFacebookSharing() {
+    let quote = this.getFacebookShareQuote();
+    this.facebookSharing = `//www.facebook.com/sharer.php?u=${this.contributionUrl}&quote=${quote}`;
   }
 
   private getFacebookShareQuote() {
-    if (!this.contribution) {
-      return 'Corre con Ousman tu también ...';
-    }
     switch (this.contribution.sport) {
       case SportEnum.Cycling:
         return `${this.contribution.firstName} acaba de pedalear ${this.contribution.distance} km con Ousman`;
@@ -70,64 +89,30 @@ export class ThankYouComponent implements OnDestroy {
     }
   }
 
-  private getWhatsappShareQuote() {
-    if (!this.contribution) {
-      return 'Corre con Ousman tu también ...';
+  private setupWhatsappSharing() {
+    const wpShareQuote = this.getWhatsappShareQuote();
+    if (this._isMobile || this._isTablet) {
+      const encodedText = encodeURIComponent(wpShareQuote);
+      const shareUrl = `whatsapp://send?text=${encodedText}`;
+      this.whatsappSharing = this.sanitizer.bypassSecurityTrustUrl(shareUrl);
     }
+    else {
+      this.whatsappSharing = `https://web.whatsapp.com/send?text=${wpShareQuote}`;
+    }
+  }
+
+  private getWhatsappShareQuote() {
     switch (this.contribution.sport) {
       case SportEnum.Cycling:
-        return `Hola, acabo de pedalear ${this.contribution.distance} km con Ousman, véalo en ${environment.MAP_URL}?c=${this.contribution._id}`;
+        return `Hola, acabo de pedalear ${this.contribution.distance} km con Ousman, véalo en ${this.contributionUrl}`;
       case SportEnum.Swimming:
-        return `Hola, acabo de nadar ${this.contribution.distance} km con Ousman, véalo en ${environment.MAP_URL}?c=${this.contribution._id}`;
+        return `Hola, acabo de nadar ${this.contribution.distance} km con Ousman, véalo en ${this.contributionUrl}`;
       case SportEnum.Trekking:
-        return `Hola, acabo de caminar ${this.contribution.distance} km con Ousman, véalo en ${environment.MAP_URL}?c=${this.contribution._id}`;
+        return `Hola, acabo de caminar ${this.contribution.distance} km con Ousman, véalo en ${this.contributionUrl}`;
       case SportEnum.Running:
       default:
-        return `Hola, acabo de correr ${this.contribution.distance} km con Ousman, véalo en ${environment.MAP_URL}?c=${this.contribution._id}`;
+        return `Hola, acabo de correr ${this.contribution.distance} km con Ousman, véalo en ${this.contributionUrl}`;
     }
-  }
-
-  private setContributionId(id: any) {
-    this.id = id;
-    this.setContribution();
-  }
-
-  private setContribution() {
-    if (this.id && this._journeyContributions) {
-      this.contribution = this._journeyContributions.find(c => c._id == this.id);
-      const wpShareQuote = this.getWhatsappShareQuote();
-      if (this._isMobile || this._isTablet) {
-        const encodedText = encodeURIComponent(wpShareQuote);
-        const shareUrl = `whatsapp://send?text=${encodedText}`;
-        this.whatsappSharing = this.sanitizer.bypassSecurityTrustUrl(shareUrl);
-      }
-      else {
-        this.whatsappSharing = `https://web.whatsapp.com/send?text=${wpShareQuote}`;
-      }
-    }
-  }
-
-  private initFacebook() {
-    this.FB = top['FB'];
-
-    let options = {
-      appId: '410894056609523',
-      version: 'v3.2',
-      cookie: true,
-      status: true,
-      frictionlessRequests: true
-    };
-
-    this.FB.init(options);
-  }
-
-  private subscribeJourneyContributions() {
-    let sub = this.store.select(fromRoot.getJourneyContributions)
-      .subscribe((contributions: JourneyContribution[]) => {
-        this._journeyContributions = contributions;
-        this.setContribution();
-      });
-    this._subscriptions.push(sub);
   }
 
 }
